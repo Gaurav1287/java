307de664e155efef4b3b33a94ddc595d3132708c
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-09-15 11:05:57 +0530

    Add files via upload

--- DsaCode/Smallest.java
+++ DsaCode/Smallest.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+class Smallest
+{
+	public static void main(String[] args) {
+		int arr[]={2,4,1,3,7,8,9};
+		int samll=arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			if(arr[i]<samll)
+			{
+				samll=arr[i];
+			}
+		}
+		System.out.print(samll);
+	}
+}
